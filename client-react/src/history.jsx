1dc52f86d4781e5a1af29c68434079fc80a415d4
mtigzoe <22895511+mtigzoe@users.noreply.github.com>
2026-09-18 22:46:21 -0400

    chore: remove unused history navigation import

--- client-react/src/history.jsx
+++ client-react/src/history.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './App.css';
 import './accessibility.css';
-import MainNav from './components/MainNav.jsx';
 import HistoryPage from './components/HistoryPage.jsx';
 
 function HistoryApp() {
