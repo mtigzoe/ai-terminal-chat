2a9a6f2984df1ff951cf65e3467a84c7af74cc8a
mtigzoe <22895511+mtigzoe@users.noreply.github.com>
2026-09-22 15:32:25 -0400

    Block Git remote proxy and helper configuration

--- server-python/tools.py
+++ server-python/tools.py
@@ -1236,7 +1236,7 @@ _GIT_CONFIG_OVERRIDES = [
 
 
 DYNAMIC_GIT_CONFIG_KEY_RE = re.compile(
-    r"^(filter\..+\.(clean|smudge|process|required)|url\..+\.(insteadof|pushinsteadof)|include\.path|includeif\..+\.path|merge\..+\.driver|remote\..+\.(uploadpack|receivepack)|diff\..+\.textconv|submodule\..+\.update)$",
+    r"^(filter\..+\.(clean|smudge|process|required)|url\..+\.(insteadof|pushinsteadof)|include\.path|includeif\..+\.path|merge\..+\.driver|remote\..+\.(uploadpack|receivepack|proxy|vcs)|diff\..+\.textconv|submodule\..+\.update)$",
     re.IGNORECASE,
 )
 
